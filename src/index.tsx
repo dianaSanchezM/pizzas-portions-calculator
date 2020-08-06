0085d0389dfeaaf19abf4bdc73b87a51e568574b
dianaSanchezM <diana.sanchez-m@mail.escuelaing.edu.co>
2020-08-05 19:23:09 -0500

    useContext hook used

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import PizzaProvider from './PizzaProvider';
-
+//Added Pizza provider for useContext hook use only
 ReactDOM.render(
   <React.StrictMode>
     <PizzaProvider>
